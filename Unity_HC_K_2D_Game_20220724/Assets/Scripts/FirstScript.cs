b66381b9aaee7d80461d57b11b0fb219b61f3114
KID <50666207+KID421@users.noreply.github.com>
2022-08-14 11:54:56 +0800

    Update 認識 C# 程式基本

--- Unity_HC_K_2D_Game_20220724/Assets/Scripts/FirstScript.cs
+++ Unity_HC_K_2D_Game_20220724/Assets/Scripts/FirstScript.cs
@@ -1,6 +1,51 @@
 using UnityEngine;
 
-public class FirstScript : MonoBehaviour
+namespace KID
 {
-    
+    /// <summary>
+    /// 類別摘要
+    /// </summary>
+    public class FirstScript : MonoBehaviour
+    {
+        /// <summary>
+        /// 摘要，簡短說明，不是必要但很重要
+        /// </summary>
+        private void Start()
+        {
+            print("哈囉，沃德~");
+        }
+
+        /// <summary>
+        /// 更新事件
+        /// </summary>
+        private void Update()
+        {
+            print("更新事件~");
+        }
+
+        // 單行註解：標記說明
+        // KID 2022.08.14
+        // 程式區塊說明
+
+        /* 多行註解
+         * 多行註解
+         * 多行註解
+         * 多行註解
+         * 多行註解
+         * 多行註解
+         */
+
+        #region 程式區塊
+        // 程式區塊
+        // 程式區塊
+        // 程式區塊
+        // 程式區塊
+        // 程式區塊
+        // 程式區塊
+        // 程式區塊
+        // 程式區塊
+        // 程式區塊
+        #endregion
+    }
 }
+
