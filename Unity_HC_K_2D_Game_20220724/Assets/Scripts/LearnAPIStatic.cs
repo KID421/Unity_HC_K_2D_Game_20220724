d4dfafadcd2305764067c91725a21ed9069c6dd8
KID <50666207+KID421@users.noreply.github.com>
2022-08-21 17:05:06 +0800

    Update 認識非靜態 API

--- Unity_HC_K_2D_Game_20220724/Assets/Scripts/LearnAPIStatic.cs
+++ Unity_HC_K_2D_Game_20220724/Assets/Scripts/LearnAPIStatic.cs
@@ -7,8 +7,12 @@ namespace KID
     /// </summary>
     public class LearnAPIStatic : MonoBehaviour
     {
+        private Vector3 v3A = new Vector3(1, 1, 1);
+        private Vector3 v3B = new Vector3(22, 22, 22);
+
         private void Start()
         {
+            #region 認識靜態屬性
             // 靜態屬性
             // 1. 取得 Get
             // 語法：類別名稱.靜態屬性
@@ -18,14 +22,41 @@ namespace KID
             // 語法：類別名稱.靜態屬性 指定 值；
             // Random.value = 0.3f; 唯讀屬性不能設定
             Cursor.visible = false;
+            #endregion
+
+            // 練習 取得靜態屬性 1 ~ 2
+            print($"攝影機數量 { Camera.allCamerasCount }");
+            print($"平台 { Application.platform }");
+
+            // 練習 設定靜態屬性 1 ~ 2
+            Physics.sleepThreshold = 10;
+            Time.timeScale = 0.5f;
+
+            // 練習 呼叫靜態方法 1，2，4
+            print(Mathf.Round(2.5f));
+            print(Mathf.Floor(2.5f));
+            print(Mathf.Ceil(2.5f));
+
+            print($"A B 點距離 { Vector3.Distance(v3A, v3B) }");
+
+            Application.OpenURL("https://unity.com/");
         }
 
         private void Update()
         {
+            #region 認識靜態方法
             // 靜態方法
             // 3. 使用方法
             // 語法：類別名稱.靜態方法(對應引數)
-            print(Random.Range(0, 3));
+            // print(Random.Range(0, 3));
+            #endregion
+
+            // 練習 取得靜態屬性 3 ~ 4
+            // print($"是否按下任意鍵 { Input.anyKeyDown }");
+            // print($"遊戲經過時間 {Time.time }");
+            
+            // 練習 呼叫靜態方法 3
+            print($"是否按下空白鍵 {Input.GetKeyDown(KeyCode.Space) }");
         }
     }
 }
--- Unity_HC_K_2D_Game_20220724/Assets/Scripts/MoveSystem.cs
+++ Unity_HC_K_2D_Game_20220724/Assets/Scripts/MoveSystem.cs
@@ -7,6 +7,7 @@ namespace KID
     /// </summary>
     public class MoveSystem : MonoBehaviour
     {
+        #region 資料
         [SerializeField, Header("移動速度"), Range(0, 100)]
         private float speed = 3.5f;
         [SerializeField, Header("移動參數名稱")]
@@ -14,6 +15,29 @@ namespace KID
 
         private Animator ani;
         private Rigidbody2D rig;
+        #endregion
+
+        #region 方法
+        /// <summary>
+        /// 移動方法
+        /// </summary>
+        private void Move()
+        {
+
+        }
+        #endregion
+
+        #region 事件
+        private void Awake()
+        {
+            
+        }
+
+        private void Update()
+        {
+            
+        }
+        #endregion
     }
 }
 
