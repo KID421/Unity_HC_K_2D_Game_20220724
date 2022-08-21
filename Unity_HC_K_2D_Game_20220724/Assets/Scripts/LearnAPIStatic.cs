5d01ceffb6e4274c167928ad97d8c7a464eff306
KID <50666207+KID421@users.noreply.github.com>
2022-08-21 12:59:35 +0800

    Update 靜態 API

---
+++ Unity_HC_K_2D_Game_20220724/Assets/Scripts/LearnAPIStatic.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace KID
+{
+    /// <summary>
+    /// 學習 API 靜態的用法
+    /// </summary>
+    public class LearnAPIStatic : MonoBehaviour
+    {
+        private void Start()
+        {
+            // 靜態屬性
+            // 1. 取得 Get
+            // 語法：類別名稱.靜態屬性
+            print($"<color=red>{Random.value}</color>");
+
+            // 2. 設定 Set (Read Only 不能設定)
+            // 語法：類別名稱.靜態屬性 指定 值；
+            // Random.value = 0.3f; 唯讀屬性不能設定
+            Cursor.visible = false;
+        }
+
+        private void Update()
+        {
+            // 靜態方法
+            // 3. 使用方法
+            // 語法：類別名稱.靜態方法(對應引數)
+            print(Random.Range(0, 3));
+        }
+    }
+}
--- Unity_HC_K_2D_Game_20220724/Assets/Scripts/LearnMethod.cs
+++ Unity_HC_K_2D_Game_20220724/Assets/Scripts/LearnMethod.cs
@@ -33,7 +33,8 @@ namespace KID
             AddTen(7);
             Add(100, 300);
             Add(50, 999);
-            Skill("火球");
+            // 火球，咻咻咻，爆炸
+            Skill("火球", effect: "爆炸");
             Skill("冰球");
             Skill("電球", "滋滋滋");
         }
@@ -57,10 +58,11 @@ namespace KID
 
         // 施放技能：火球、冰球、電球...
         // 有預設值的參數：選擇式參數 - 必須寫在最右邊，呼叫時不填會以預設值帶入
-        private void Skill(string skillType, string sound = "咻咻咻")
+        private void Skill(string skillType, string sound = "咻咻咻", string effect = "碎片")
         {
             print("施放技能特效：" + skillType);
             print("技能音效：" + sound);
+            print($"技能附帶特效： {effect}");
         }
     }
 }
